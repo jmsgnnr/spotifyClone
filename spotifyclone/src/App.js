663b746a8aff10668a8b8b175f9dfc53001079a9
jmsgnnr <jmsgnnr@gmail.com>
2021-03-22 21:01:57 -0600

    Made changes in app.js to access spotify API - using implicit auth from spotify - added file for hash - installed packages for jquery access

--- spotifyclone/src/App.js
+++ spotifyclone/src/App.js
@@ -1,25 +1,87 @@
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
+import hash from "./hash.js"
+import $ from "jquery";
+// import Player from "./Player";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+const clientId= "d433a2314c9547a987c04c5516cbefe6";
+const redirectUri = "http://localhost:3000";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      token: null,
+    item: {
+      album: {
+        images: [{url : ""}]
+      },
+      name: "",
+      artists: [{name: ""}],
+      duration_ms: 0,
+    },
+    is_playing: "Paused",
+    progress_ms: 0,    
+  };
+
+  this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
+  }
+
+  getCurrentlyPlaying(token) {
+  $.ajax({
+    url: "https://spotify.com/v1/me/player",
+    type: "GET",
+    beforeSend: (xhr) => {
+      xhr.setRequestHeader("Authorization", "Bearer " + token);
+    },
+    success: (data) => {
+      this.setState({
+        item: data.item,
+        is_playing: data.is_playing,
+        progress_ms: data.progress_ms,
+      });
+    }
+  });
+  }
+    componentDidMount() {
+      let _token = hash.access_token;
+      if (_token) {
+        this.setState({
+          token : _token
+        });
+      }
+    }
+
+    render() {
+      return (
+        <div className="App">
+          <header className="App-Header">
+            <img src={logo} className="App-logo" alt="logo" />
+            {!this.state.token && (
+              <a
+                className="btn btn--loginApp-link"
+                href=
+                {`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+                  >
+                    Login To Spotify!
+                  </a>
+            )}
+            {/* {this.state.token && (
+              // spotify player will go here. 
+            )} */}
+            </header>
+            </div>
+      );
+    }
+
+
+  }
 
 export default App;
